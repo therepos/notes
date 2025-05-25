c87587017f2ac2d1ec91098cfb4b894ff1fc9582
The Repos <ry4nyeo@gmail.com>
2025-05-25 16:32:51 +0800

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,55 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import DocCard from '@theme/DocCard';
 
-const docCards = [
-  {
-    type: 'link',
-    label: 'Home',
-    href: '/home',
-    description: 'Overview and essentials.',
-  },
-  {
-    type: 'link',
-    label: 'CISA',
-    href: '/cisa',
-    description: 'CISA learning and exam notes.',
-  },
-  {
-    type: 'link',
-    label: 'Blog',
-    href: '/blog',
-    description: 'Thoughts, updates, and more.',
-  },
-];
+import { useEffect } from 'react';
+import { useHistory } from '@docusaurus/router';
 
-export default function Home() {
-  return (
-    <Layout title="Welcome" description="This is a sample Docusaurus homepage">
-      <main className="homepage-container">
-        <div className="row">
-          {docCards.map((item, idx) => (
-            <div key={idx} className="col col--4 margin-bottom--lg">
-              <DocCard item={item} />
-            </div>
-          ))}
-        </div>
-      </main>
-    </Layout>
-  );
+export default function RedirectHome() {
+  const history = useHistory();
+
+  useEffect(() => {
+    history.replace('/home');
+  }, [history]);
+
+  return null;
 }
+
+
+// import React from 'react';
+// import Layout from '@theme/Layout';
+// import DocCard from '@theme/DocCard';
+
+// const docCards = [
+//   {
+//     type: 'link',
+//     label: 'Home',
+//     href: '/home',
+//     description: 'Overview and essentials.',
+//   },
+//   {
+//     type: 'link',
+//     label: 'CISA',
+//     href: '/cisa',
+//     description: 'CISA learning and exam notes.',
+//   },
+//   {
+//     type: 'link',
+//     label: 'Blog',
+//     href: '/blog',
+//     description: 'Thoughts, updates, and more.',
+//   },
+// ];
+
+// export default function Home() {
+//   return (
+//     <Layout title="Welcome" description="This is a sample Docusaurus homepage">
+//       <main className="homepage-container">
+//         <div className="row">
+//           {docCards.map((item, idx) => (
+//             <div key={idx} className="col col--4 margin-bottom--lg">
+//               <DocCard item={item} />
+//             </div>
+//           ))}
+//         </div>
+//       </main>
+//     </Layout>
+//   );
+// }
