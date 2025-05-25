48ad2b3188ef0c3eaba6e771f6c02b7837907b38
The Repos <ry4nyeo@gmail.com>
2025-05-25 16:14:04 +0800

    updates

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+
+export default function Home() {
+  return (
+    <Layout
+      title="Welcome"
+      description="This is a sample Docusaurus homepage"
+    >
+      <main style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Welcome to My Docs Site</h1>
+        <p>This page is built using Docusaurus.</p>
+        <Link to="/home">
+          <button>Get Started</button>
+        </Link>        
+      </main>
+    </Layout>
+  );
+}
