26175a71af23c79006464355346a3bb3b3ece9a4
The Repos <ry4nyeo@gmail.com>
2025-05-27 22:31:16 +0800

    updated

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDoc } from '@docusaurus/theme-common/internal';
 import clsx from 'clsx';
-import styles from './styles.module.css';
 
 export default function DocItemFooter() {
   const { metadata } = useDoc();
