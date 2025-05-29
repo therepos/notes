409a1a302438706e2563bfeb6a1f4ef7a32c66d5
The Repos <ry4nyeo@gmail.com>
2025-05-29 20:00:11 +0800

    u

--- src/theme/BlogPostItem/Footer/index.js
+++ src/theme/BlogPostItem/Footer/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import TagsListInline from '@theme/TagsListInline';
-import type { Props } from '@theme/BlogPostItem/Footer';
 
-export default function BlogPostItemFooter(props: Props): JSX.Element {
+export default function BlogPostItemFooter(props) {
   const { tags } = props;
-
   const hasTags = tags.length > 0;
 
   return (
     <>
-      {hasTags && (
-        <TagsListInline tags={tags} />
-      )}
+      {hasTags && <TagsListInline tags={tags} />}
     </>
   );
 }
