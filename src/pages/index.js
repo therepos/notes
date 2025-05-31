cbfa9dd5097d7045f3d94a12bb7da962ad20090c
The Repos <ry4nyeo@gmail.com>
2025-05-31 22:07:30 +0800

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Redirect() {
   const history = useHistory();
-  const target = useBaseUrl('/docs/Home/index.md');
-
+  const target = useBaseUrl('/docs/Home/index.md');  // use /blog for redirect to blog
+  
   useEffect(() => {
     history.replace(target);
   }, [target]);
